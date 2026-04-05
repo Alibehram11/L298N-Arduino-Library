1bb1e42d023d99f2c4328abb13de812d29ca1442
Ali Albarak <alibehram11.albarktar@gmail.com>
2026-04-06 00:18:00 +0300

    Ekle: left() ve right() fonksiyonları motor sınıfına

--- motor.h
+++ motor.h
@@ -5,20 +5,24 @@
 
 class Motor {
 public:
-    Motor(int in1, int in2, int in3, int in4, int enb, int enA) : _in1(in1), _in2(in2), _en(enA) {
+    Motor(int in1, int in2, int in3, int in4, int enb, int ena) : _in1(in1), _in2(in2), _enb(enb), _in3(in3), _in4(in4), _ena(ena) {
         pinMode(_in1, OUTPUT);
         pinMode(_in2, OUTPUT);
-        pinMode(_en, OUTPUT);
-        pinMode(in3, OUTPUT);
-        pinMode(in4, OUTPUT);   
+        pinMode(_enb, OUTPUT);
+        pinMode(_in3, OUTPUT);
+        pinMode(_in4, OUTPUT); 
+        pinMode(_ena, OUTPUT);
+   
     }
     void forward(int speed);
     void backward(int speed);
+    void left(int speed);
+    void right(int speed);
     void stop();
     void brake();
 
 private:
-    int _in1, _in2, _en;
+    int _in1, _in2, _enb, _in3, _in4, _ena;
 };
 
 #endif
